eb9f925116a5a954c43513b24abea3b8ea768d46
Ehsan Yousefi <647@samenea.com>
2019-02-17 13:48:52 +0330

    test speed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            this is CI/CD
+            this is CI/CD test Speed
           </p>
           <a
             className="App-link"
